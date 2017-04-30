1d48abb25ce75827d8c1b23417f53c845c69853b
Sandeep Kumar <sandeep.kumar82945@gmail.com>
2017-04-30 19:27:20 +0530

    Merged the NER files

--- src/python/NER.py
+++ src/python/NER.py
@@ -8,11 +8,17 @@ from read_data import read_sentence
 st = StanfordNERTagger('../../stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz','../../stanford-ner/stanford-ner.jar',encoding='utf-8')
 
 text = read_sentence()
-print text
+
 
 tokenized_text = word_tokenize(text)
 classified_text = st.tag(tokenized_text)
 
-print(classified_text)
+result=[]
 
+for a,b in classified_text:
+	if(b=='LOCATION'):
+		result.append(a[1:])
+	
+print('\n')
+print(result)
 
