1d8f0cb91b445841594cb77a38fe81640fd999ec
Sandeep Kumar <sandeep.kumar82945@gmail.com>
2017-04-30 19:23:34 +0530

    Adding stanford ner

---
+++ src/python/read_data.py
@@ -0,0 +1,14 @@
+#Read data from stdin
+import sys
+
+def read_sentence():
+  line = sys.stdin.readline()
+  #Since our input would only be having one line, parse our JSON data from that
+  return line
+
+
+
+def read_sentence1():
+  line = sys.stdin.readline()
+  #Since our input would only be having one line, parse our JSON data from that
+  return line
