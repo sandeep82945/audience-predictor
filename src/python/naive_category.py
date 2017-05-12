e03e06f490589d054235b3a8eb4791121c837cc1
Sandeep <sandeep.kumar82945@gmail.com>
2017-05-12 16:59:49 +0530

    category prediction using naive bayers

---
+++ src/python/naive_category.py
@@ -0,0 +1,58 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from read_data import read_sentence
+from read_data import convert
+
+#for reading file
+import pandas as pd
+sms = pd.read_table('pk.tsv', header=None, names=['label', 'message'])
+categories = {'BEAUTY':3, "ELECTRONICS":2, 'not Identified':0}
+
+# convert label to a numerical variable
+sms['label_num'] = sms.label.map(categories)
+
+ #check that the conversion worked
+print(sms.tail(10))
+
+X_train = sms.message
+y_train = sms.label_num
+
+
+vect = CountVectorizer()
+vect.fit(X_train)
+
+X_train_dtm = vect.transform(X_train)
+
+from sklearn.naive_bayes import MultinomialNB
+
+nb=MultinomialNB()
+
+nb.fit(X_train_dtm,y_train)
+
+text = read_sentence1()
+
+simple_test=[text]
+
+simple_test_dtm=vect.transform(simple_test)
+
+y_pred_class = nb.predict(simple_test_dtm)
+
+print convert([categories[y_pred_class[0]]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
