9e1a7789c9411cb635dbcad59e5d1b549eed93e9
Sandeep <sandeep.kumar82945@gmail.com>
2017-05-20 18:26:46 +0530

    naive score prog

---
+++ src/python/naive_category_score.py
@@ -0,0 +1,74 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split  
+from sklearn import metrics
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+import numpy as np
+
+#for reading file
+import pandas as pd
+sms = pd.read_table('data/pk.tsv', header=None, names=['label', 'message'])
+
+#Randomising the rows in the file
+sms = sms.reindex(np.random.permutation(sms.index))
+
+from categories import tsv_labels
+labels = tsv_labels['categories']["ids"]
+categories = tsv_labels['categories']["names"]
+
+sms['label_num'] = sms.label.map(labels)
+
+ #check that the conversion worked
+#print(sms.tail(10))
+
+X_train = sms.message
+y_train = sms.label_num
+
+X_train, X_test, y_train, y_test = train_test_split(sms.message, sms.label_num,test_size=0.33, random_state=42)
+
+vect = CountVectorizer()
+
+vect.fit(X_train)
+X_train_dtm = vect.transform(X_train)
+
+from sklearn.naive_bayes import MultinomialNB
+
+nb=MultinomialNB()
+
+#print X_train_dtm
+#print "Y train ========"
+#print y_train
+nb.fit(X_train_dtm,y_train)
+
+
+
+simple_test_dtm=vect.transform(X_test)
+
+y_pred_class = nb.predict(simple_test_dtm)
+
+
+
+#accurcy score
+accuracy=metrics.accuracy_score(y_test, y_pred_class)
+print "acuuracy=",accuracy
+precision=precision_score(y_test, y_pred_class, average='macro')
+print "precision=",precision
+
+recall=recall_score(y_test, y_pred_class, average='macro') 
+print "recall=",recall
+
+F1 = 2 * (precision * recall) / (precision + recall)
+
+print "f-score=",F1
+
+print metrics.classification_report(y_test, y_pred_class)
+
+
+
+
+
+
+
+
+
+
