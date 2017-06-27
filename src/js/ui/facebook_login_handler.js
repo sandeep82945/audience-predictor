c728bf75cb4171ac97872f1ca2eb3444133fd85e
Sandeep Kumar <sandeep.kumar82945@gmail.com>
2017-06-27 15:48:13 +0530

    Added function for facebook access token setting

--- src/js/ui/facebook_login_handler.js
+++ src/js/ui/facebook_login_handler.js
@@ -5,15 +5,19 @@ let ipc = global.require('electron').ipcRenderer
 // let getloginurl = () =>{
 //   return `https://graph.facebook.com/v2.9/oauth/access_token?grant_type=client_credentials&app_id=${consts.facebook.application_id}&client_id=${consts.facebook.application_id}&redirect_uri=${consts.facebook.redirect_url}`
 // }
-
-
+global.setAccessToken = (token) =>{
+  global.access_token = token
+  let fbapi = new FBApi(token)
+  FB.api('/me', { fields: ['id', 'name', 'picture.width(800).height(800)'] },  (res)  => {
+    console.log(" Name: " + res.name )
+    // mainWindow.webContents.executeJavaScript("document.getElementById(\"fb-name\").innerHTML = \" Name: " + res.name + "\"");
+    // mainWindow.webContents.executeJavaScript("document.getElementById(\"fb-id\").innerHTML = \" ID: " + res.id + "\"");
+    // mainWindow.webContents.executeJavaScript("document.getElementById(\"fb-dp\").src = \"" + res.picture.data.url + "\"");
+  });
+}
 
 module.exports = () =>{
   ipc.send("facebook-button-clicked", "yes");
-  ipc.on("", "yes");
-  ipc.on("set-access-token",(event, arg) => {
-    console.log(arg);
-  })
   //let url = getloginurl()
 
   // var info = new facebook("615566271976367","508f0e61a63298135974ea17d3aa71e4", "public_profile", {redirect_url: 'https://www.facebook.com/connect/login_success.html'});
