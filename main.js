680665625a98dc78267c9d6f99764edd187d3714
Sandeep Kumar <sandeep.kumar82945@gmail.com>
2017-06-27 16:11:53 +0530

    Fixed login with facebook

--- main.js
+++ main.js
@@ -59,10 +59,13 @@ ipc.on("facebook-button-clicked",function (event, arg) {
     scopes: "public_profile&ads_read&adsmanagement",
     redirect_uri: "https://www.facebook.com/connect/login_success.html"
   };
-  var authWindow = new BrowserWindow({ width: 450, height: 300, show: false, 'node-integration': false });
+  var authWindow = new BrowserWindow({ width: 450, height: 300, show: false, webPreferences:{ nodeIntegration: false }});
   var facebookAuthURL = "https://www.facebook.com/dialog/oauth?client_id=" + options.client_id + "&redirect_uri=" + options.redirect_uri + "&response_type=token,granted_scopes&scope=" + options.scopes + "&display=popup";
+
+  //authWindow.webContents.executeJavaScript("window.require=null");
   authWindow.loadURL(facebookAuthURL);
   authWindow.show();
+  //authWindow.webContents.executeJavaScript("$()window.close()");
   authWindow.webContents.on('did-get-redirect-request', function (event, oldUrl, newUrl) {
     var raw_code = /access_token=([^&]*)/.exec(newUrl) || null;
     access_token = (raw_code && raw_code.length > 1) ? raw_code[1] : null;
